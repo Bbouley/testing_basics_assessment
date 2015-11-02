714bdc01ab94c3da9a5abf66d0f9bc5b4da1d738
bbouley <b.bouley@hotmail.co.uk>
2015-11-02 14:28:15 -0700

    added in modescore function

--- grader-test.spec.js
+++ grader-test.spec.js
@@ -24,11 +24,33 @@ describe('grader()', function() {
     });
 });
 
-// describe('averageScore()', function() {
-//     it('should find average score', function() {
-//         expect(grader.averageScore([10,20,15,15])).to.equal(15);
-//         expect(grader.averageScore([90,95,87,60])).to.equal(83);
-//         expect(grader.averageScore([80,82,84,86])).to.equal(83);
-//         expect(grader.averageScore([87,90,60,23,59,99])).to.equal(70);
-//     });
-// });
+describe('averageScore()', function() {
+    it('should find average score', function() {
+        expect(grader.averageScore([10,20,15,15])).to.equal(15);
+        expect(grader.averageScore([90,95,87,60])).to.equal(83);
+        expect(grader.averageScore([80,82,84,86])).to.equal(83);
+        expect(grader.averageScore([87,90,60,23,59,99])).to.equal(70);
+        expect(grader.averageScore(['l',90,60,null,59,99])).to.equal(77);
+        expect(grader.averageScore([0, 0])).to.equal(0);
+    });
+});
+
+describe('medianScore()', function() {
+    it('should find median score', function() {
+        expect(grader.medianScore([1,2,3,4,5])).to.equal(3);
+        expect(grader.medianScore([2,4,6,8,10,12])).to.equal(7);
+        expect(grader.medianScore([8,10,6,4,2,12])).to.equal(7);
+        expect(grader.medianScore([1,2,3,4,5,6])).to.equal(3.5);
+        expect(grader.medianScore([80,7,90,45,32])).to.equal(45);
+        expect(grader.medianScore(['A',2,null,4,undefined])).to.equal(3);
+    });
+});
+
+describe('modeScore()', function() {
+    it('should find mode number', function() {
+        expect(grader.modeScore([1,2,1,3,2,2,4,5])).to.equal(2);
+        expect(grader.modeScore([98,67,88,23,4,23])).to.equal(23);
+        expect(grader.modeScore([1,2,3,4,5])).to.equal('No Mode');
+        expect(grader.modeScore(['P',67,88,23,4,23])).to.equal(23);
+    });
+});
--- grader.js
+++ grader.js
@@ -17,14 +17,87 @@ function letterGrader(score) {
     }
 }
 
-// function averageScore(array) {
-//     var nums = array.length;
-//     var total =
+function averageScore(array) {
+    var nums = 0;
+    var total = 0;
+    for (var i = 0; i < array.length; i++) {
+        if(typeof array[i] === 'number' && array[i] !== null) {
+            total += array[i];
+            nums += 1;
+        }
+    }
+    if(nums === 0 && total === 0) {
+        return 0;
+    } else {
+        var output = Math.round(total/nums);
+        return output;
+    }
+}
 
-// }
+function medianScore(array) {
+    var sortedArray = getNums(array).sort(sortNumber);
+    if(sortedArray.length % 2 === 0) {
+        var indexTop = (sortedArray.length/2);
+        var indexBottom = (sortedArray.length/2) - 1 ;
+        var output = (sortedArray[indexTop] + sortedArray[indexBottom]) / 2;
+        return output;
+    } else {
+        var index = Math.floor(array.length/2);
+        return sortedArray[index];
+    }
+}
+
+function modeScore(array) {
+    var count = {};
+    var numArray = getNums(array);
+    for (var i = 0; i < numArray.length; i++) {
+        if(count[numArray[i]] === undefined) {
+            var value = 1;
+            count[numArray[i]] = value;
+        } else {
+            count[numArray[i]] += 1;
+        }
+    }
+
+    var score = 0;
+    var num = 0;
+
+    for (var j in count) {
+        if(count[j] > score) {
+            score = count[j];
+            num = j;
+        }
+    }
+
+    if(score === 1) {
+        return 'No Mode';
+    } else {
+        return parseInt(num);
+    }
+}
+
+
+// *** helper functions *** //
+
+function sortNumber(a, b) {
+    return a - b;
+}
+
+function getNums(array) {
+    var onlyNums = [];
+    for (var i = 0; i < array.length; i++) {
+        if(typeof array[i] === 'number') {
+            onlyNums.push(array[i]);
+        }
+    }
+    return onlyNums;
+}
+
 
 module.exports = {
     letterGrader : letterGrader,
-    // averageScore :averageScore
+    averageScore :averageScore,
+    medianScore : medianScore,
+    modeScore : modeScore
 };
 
